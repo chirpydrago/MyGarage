7809a7297f6c1509f09a1c1e3369c8bdb0378316
Lawrence Wong <lawrencewong.dev@gmail.com>
2021-06-25 19:22:08 +0800

    last commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,24 @@
-import React from "react";
+//import React from "react";
+import React, { useState, useEffect } from "react";
+import { Contract, getDefaultProvider, providers, utils } from "ethers";
+import { config } from "../config";
+import abi from "../fixtures/abi.json";
+import axios from "axios";
 
-const MOCK_DATA = [
+
+
+const formatIpfsUrl = (url) => {
+  return url.replace(/ipfs:\/\//g, "https://cloudflare-ipfs.com/");
+};
+
+const provider = getDefaultProvider("rinkeby",{alchemy: config.alchemyKey});
+const contract = new Contract(
+  "0xE355ed57069bf69Ade5Bd85fEb22349306985Cf7",
+  abi,
+  provider
+);
+
+/* const MOCK_DATA = [
   {
     id: 0,
     image:
@@ -25,7 +43,7 @@ const MOCK_DATA = [
     description: "Pick Me. Pick Me.",
     owner: "0xE355ed57069bf69Ade5Bd85fEb22349306985Cf7",
   },
-];
+]; */
 
 /* export const HomePage = () => {
   const handlePurchase = () => {
@@ -42,33 +60,93 @@ const MOCK_DATA = [
 }; */
 
 export const HomePage = () => {
-  const handlePurchase = () => {
-    alert("Purchasing a Robot...");
+
+  const [mintedNftState, setMintedNftState] = useState({
+    state: "UNINITIALIZED",
+  });
+
+  const loadRobotsData = async () => {
+    setMintedNftState({
+      state: "PENDING",
+    });
+    const totalSupply = await contract.totalSupply();
+    const ids = [...Array(totalSupply.toNumber()).keys()];
+    const deferredData = ids.map(async (id) => {
+      const ipfsUri = await contract.tokenURI(id);
+      const owner = await contract.ownerOf(id);
+      const formattedUri = formatIpfsUrl(ipfsUri);
+      const metadata = (await axios.get(formattedUri)).data;
+      const formattedImage = formatIpfsUrl(metadata.image);
+      return {
+        id,
+        name: metadata.name,
+        image: formattedImage,
+        description: metadata.description,
+        owner,
+      };
+    });
+    const data = await Promise.all(deferredData);
+    setMintedNftState({
+      state: "SUCCESS",
+      data,
+    });
+  };
+
+  useEffect(() => {
+    loadRobotsData();
+  }, []);
+
+
+  const handlePurchase = async () => {
+    const { ethereum } = window;
+    if (typeof ethereum == "undefined") alert("Metamask is not detected");
+    
+    // Prompts Metamask to connect
+    await ethereum.enable();
+  
+    // Create new provider from Metamask
+    const provider = new providers.Web3Provider(window.ethereum);
+  
+    // Get the signer from Metamask
+    const signer = provider.getSigner();
+    
+    // Sends a simple transaction to an account
+    await signer.sendTransaction({
+      to: "0xA4bFA6B9819bc8857524eA5a6C691F045131307b",
+      value: utils.parseEther("0.01"),
+    });
   };
 
   return (
     <div className="min-h-screen bg-gray-800">
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
         <div className="text-gray-100 text-6xl pt-28 pb-10">ROBOTS</div>
-        <div className="grid grid-cols-3 gap-4">
-          {MOCK_DATA.map(({ id, image, name, description, owner }) => {
-            return (
-              <div key={id} className="bg-white rounded p-2">
-                <img src={image} className="mx-auto p-4" alt="Robots"  />
-                <div className="text-xl">{name}</div>
-                <div className="">{description}</div>
-                <hr className="my-4" />
-                <div className="text-left text-sm">Owned By:</div>
-                <div className="text-left text-xs">{owner}</div>
-              </div>
-            );
-          })}
-        </div>
+        {mintedNftState.state === "PENDING" && (
+          <div className="text-xl text-white">LOADING...</div>
+        )}
+        {mintedNftState.state === "SUCCESS" && (
+          <div className="grid grid-cols-3 gap-4">
+            {mintedNftState.data.map(
+              ({ id, image, name, description, owner }) => {
+                return (
+                  <div key={id} className="bg-white rounded p-2">
+                    <img src={image} className="mx-auto p-4" alt={name} />
+                    <div className="text-xl">{name}</div>
+                    <div className="">{description}</div>
+                    <hr className="my-4" />
+                    <div className="text-left text-sm">Owned By:</div>
+                    <div className="text-left text-xs">{owner}</div>
+                  </div>
+                );
+              }
+            )}
+          </div>
+        )}
         <div className="mt-12">
           <button
             onClick={handlePurchase}
             type="button"
-            class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
           >
             Buy My Robot
           </button>
